d68427819673dc4a5f2d56cf46d36941ddc2c52a
marshaldevs <marshaldevs@gmail.com>
2016-01-09 22:35:56 +0200

    Initial commit

---
+++ app/src/main/java/com/basmach/marshal/MainActivity.java
@@ -0,0 +1,119 @@
+package com.basmach.marshal;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fm = getFragmentManager();
+
+        if (id == R.id.nav_courses) {
+            fm.beginTransaction().replace(R.id.content_frame, new CoursesFragment()).commit();
+        } else if (id == R.id.nav_materials) {
+            fm.beginTransaction().replace(R.id.content_frame, new MaterialsFragment()).commit();
+        } else if (id == R.id.nav_discussions) {
+            fm.beginTransaction().replace(R.id.content_frame, new DiscussionsFragment()).commit();
+        } else if (id == R.id.nav_malshab) {
+            fm.beginTransaction().replace(R.id.content_frame, new MalshabFragment()).commit();
+        } else if (id == R.id.nav_settings) {
+            Intent getSettingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(getSettingsIntent);
+        } else if (id == R.id.nav_contact_us) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+            intent.putExtra(android.content.Intent.EXTRA_EMAIL,new String[] { "marshaldevs@gmail.com" });
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        } else if (id == R.id.nav_about) {
+            CustomTabsIntent.Builder customTabsIntent = new CustomTabsIntent.Builder();
+            customTabsIntent.setShowTitle(true);
+            int color = getResources().getColor(R.color.colorPrimary);
+            customTabsIntent.setToolbarColor(color);
+            String url = "https://goo.gl/s6thV1";
+            customTabsIntent.build().launchUrl(this, Uri.parse(url));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/basmach/marshal/MalshabFragment.java
@@ -0,0 +1,17 @@
+package com.basmach.marshal;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MalshabFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.malshab_fragment, container, false);
+        return rootView;
+    }
+}
