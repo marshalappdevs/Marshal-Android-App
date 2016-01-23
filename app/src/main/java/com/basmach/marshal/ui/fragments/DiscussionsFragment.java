ec340c85566058dc505b411d41ae3b2d2a582785
marshaldevs <marshaldevs@gmail.com>
2016-01-23 12:17:32 +0200

    removed unnecessary stuff from fragments

--- app/src/main/java/com/basmach/marshal/ui/fragments/DiscussionsFragment.java
+++ app/src/main/java/com/basmach/marshal/ui/fragments/DiscussionsFragment.java
@@ -1,7 +1,6 @@
 package com.basmach.marshal.ui.fragments;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,18 +15,8 @@ import com.basmach.marshal.R;
  * Activities that contain this fragment must implement the
  * {@link DiscussionsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link DiscussionsFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class DiscussionsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,32 +24,6 @@ public class DiscussionsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DiscussionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DiscussionsFragment newInstance(String param1, String param2) {
-        DiscussionsFragment fragment = new DiscussionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
