3df6e4879a627157f38504c035d3954d69ef36de
marshadevs <marshaldevs@gmail.com>
2016-01-24 18:29:56 +0200

    added flag secure added missing splash icons

--- app/src/main/java/com/basmach/marshal/ui/MainActivity.java
+++ app/src/main/java/com/basmach/marshal/ui/MainActivity.java
@@ -30,6 +30,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -62,6 +63,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_main);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
