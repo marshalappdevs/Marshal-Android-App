b45104635903334e373c5cba0d616963b81bfb6a
marshadevs <marshaldevs@walla.com>
2016-02-29 19:05:57 +0200

    cleanups

--- app/src/main/java/com/basmach/marshal/ui/fragments/CoursesFragment.java
+++ app/src/main/java/com/basmach/marshal/ui/fragments/CoursesFragment.java
@@ -100,10 +100,6 @@ public class CoursesFragment extends Fragment {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        if (mTimer != null) {
-                            mTimer.cancel();
-                            mTimer = null;
-                        }
                         startImagesTimer();
                     }
                 }, 2000);
