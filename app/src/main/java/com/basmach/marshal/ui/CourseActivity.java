8c897e7857e78141c1d17781152cc3b173d48e7f
marshaldevs <marshaldevs@gmail.com>
2016-03-12 02:56:57 +0200

    Added theme change to course activity Toolbar and statusbar color based on course image

--- app/src/main/java/com/basmach/marshal/ui/CourseActivity.java
+++ app/src/main/java/com/basmach/marshal/ui/CourseActivity.java
@@ -3,29 +3,39 @@ package com.basmach.marshal.ui;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.basmach.marshal.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.Locale;
 
 public class CourseActivity extends AppCompatActivity {
+    private CollapsingToolbarLayout collapsingToolbarLayout;
     private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        updateTheme();
+        super.onCreate(savedInstanceState);
         updateLocale();
         setContentView(R.layout.activity_course);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -39,12 +49,22 @@ public class CourseActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+
+        final ImageView header = (ImageView) findViewById(R.id.header);
+        Picasso.with(this).load(R.drawable.bg_empty_profile_art).into(header, new Callback() {
+            @Override public void onSuccess() {
+                Bitmap bitmap = ((BitmapDrawable) header.getDrawable()).getBitmap();
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    public void onGenerated(Palette palette) {
+                        collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary)));
+                        collapsingToolbarLayout.setStatusBarScrimColor(ContextCompat.getColor(getApplicationContext(), R.color.black_trans80));
+                    }
+                });
+            }
+
+            @Override public void onError() {
+
             }
         });
     }
@@ -53,6 +73,21 @@ public class CourseActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         updateLocale();
+    }
+
+    private void updateTheme() {
+        String theme = mSharedPreferences.getString("THEME", "light");
+        if (theme.equals("light")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        if (theme.equals("dark")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        if (theme.equals("auto")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+        }
+        getDelegate().applyDayNight();
+        setTheme(R.style.AppTheme);
     }
 
     private void updateLocale() {
