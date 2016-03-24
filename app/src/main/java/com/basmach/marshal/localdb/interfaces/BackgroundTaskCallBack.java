1782d641e0f60a41f5533ad06bb54a6abf0d4636
marshaldevs <marshaldevs@gmail.com>
2016-03-24 20:38:57 +0200

    minor changes

--- app/src/main/java/com/basmach/marshal/localdb/interfaces/BackgroundTaskCallBack.java
+++ app/src/main/java/com/basmach/marshal/localdb/interfaces/BackgroundTaskCallBack.java
@@ -2,9 +2,6 @@ package com.basmach.marshal.localdb.interfaces;
 
 import java.util.List;
 
-/**
- * Created by Ido on 10/24/2015.
- */
 public interface BackgroundTaskCallBack {
     void onSuccess(String result, List<Object> data);
     void onError(String error);
