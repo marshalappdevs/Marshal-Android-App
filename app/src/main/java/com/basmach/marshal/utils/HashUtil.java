24f7ce8b62e9ee33d9f2f5c09f933eee9572f2f3
shnizlon <orshnizelon@gmail.com>
2016-05-17 17:21:01 +0300

    improved hash method

--- app/src/main/java/com/basmach/marshal/utils/HashUtil.java
+++ app/src/main/java/com/basmach/marshal/utils/HashUtil.java
@@ -5,30 +5,27 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashUtil {
-    public static String SHA1(String text) {
+
+    public static String SHA1(String toHash) {
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance( "SHA-1" );
-            messageDigest.update(text.getBytes("UTF-8"), 0, text.length());
-            byte[] sha1hash = messageDigest.digest();
-            return toHex(sha1hash);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+            messageDigest.update(toHash.getBytes("UTF-8"), 0, toHash.length());
+            byte[] bytes = messageDigest.digest();
+            return bytesToHex(bytes);
+        } catch( NoSuchAlgorithmException | UnsupportedEncodingException e ) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static String toHex(byte[] buf) {
-        if (buf == null) return "";
-        StringBuffer result = new StringBuffer(buf.length * 2);
-        for (byte aBuf : buf) {
-            appendHex(result, aBuf);
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[ bytes.length * 2 ];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[ j ] & 0xFF;
+            hexChars[ j * 2 ] = hexArray[ v >>> 4 ];
+            hexChars[ j * 2 + 1 ] = hexArray[ v & 0x0F ];
         }
-        return result.toString();
-    }
-
-    final protected static String HEX = "0123456789ABCDEF";
-    private static void appendHex(StringBuffer sb, byte b) {
-        sb.append(HEX.charAt((b >> 4) & 0x0f))
-                .append(HEX.charAt(b & 0x0f));
+        return new String(hexChars);
     }
 }
