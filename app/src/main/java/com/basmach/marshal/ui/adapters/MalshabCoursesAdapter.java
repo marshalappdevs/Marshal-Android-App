66450b54c6afe626480181dfb7287eb4b4d3b165
marshaldevs <marshaldevs@gmail.com>
2016-05-21 17:49:15 +0300

    added links to malshab fragment

--- app/src/main/java/com/basmach/marshal/ui/adapters/MalshabCoursesAdapter.java
+++ app/src/main/java/com/basmach/marshal/ui/adapters/MalshabCoursesAdapter.java
@@ -1,13 +1,20 @@
 package com.basmach.marshal.ui.adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.basmach.marshal.R;
 import com.squareup.picasso.Picasso;
@@ -19,21 +26,28 @@ public final class MalshabCoursesAdapter extends BaseAdapter {
     private Context mContext;
     private final List<Item> mItems = new ArrayList<>();
     private final LayoutInflater mInflater;
+    private SharedPreferences mSharedPreferences;
 
     public MalshabCoursesAdapter(Context context) {
         this.mContext = context;
+        this.mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
         mInflater = LayoutInflater.from(context);
 
         mItems.add(new Item(R.string.programmer,
-                "http://www.tikshuv.idf.il/Sip_Storage/FILES/2/1532.jpg"));
+                "http://www.tikshuv.idf.il/Sip_Storage/FILES/2/1532.jpg",
+                "http://www.tikshuv.idf.il/1048-7486-HE/tikshuv.aspx"));
         mItems.add(new Item(R.string.infrastructure_manager,
-                "http://www.tikshuv.idf.il/Sip_Storage/FILES/9/1539.jpg"));
+                "http://www.tikshuv.idf.il/Sip_Storage/FILES/9/1539.jpg",
+                "http://www.tikshuv.idf.il/1048-7490-HE/tikshuv.aspx"));
         mItems.add(new Item(R.string.software_qa,
-                "http://www.tikshuv.idf.il/Sip_Storage/FILES/3/1533.jpg"));
+                "http://www.tikshuv.idf.il/Sip_Storage/FILES/3/1533.jpg",
+                "http://www.tikshuv.idf.il/1048-7504-HE/tikshuv.aspx"));
         mItems.add(new Item(R.string.cyber_defender,
-                "http://www.tikshuv.idf.il/Sip_Storage/FILES/3/1523.jpg"));
+                "http://www.tikshuv.idf.il/Sip_Storage/FILES/3/1523.jpg",
+                "http://www.tikshuv.idf.il/1048-7536-HE/tikshuv.aspx"));
         mItems.add(new Item(R.string.educational_tools_developer,
-                "http://www.tikshuv.idf.il/Sip_Storage/FILES/4/1554.jpg"));
+                "http://www.tikshuv.idf.il/Sip_Storage/FILES/4/1554.jpg",
+                "http://www.tikshuv.idf.il/1048-7537-HE/tikshuv.aspx"));
     }
 
     @Override
@@ -70,16 +84,26 @@ public final class MalshabCoursesAdapter extends BaseAdapter {
 
         name.setText(item.name);
 
-        int width= mContext.getResources().getDisplayMetrics().widthPixels;
         Picasso.with(mContext)
                 .load(item.image)
-                .centerCrop().resize(width/2,width/2)
                 .into(picture);
 
         v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mContext, item.name, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, item.name, Toast.LENGTH_SHORT).show();
+                Boolean cct = mSharedPreferences.getBoolean("CCT", true);
+                if (cct) {
+                    new CustomTabsIntent.Builder()
+                            .setToolbarColor(ContextCompat.getColor(mContext, R.color.colorPrimary))
+                            .setShowTitle(true)
+                            .addDefaultShareMenuItem()
+                            .setCloseButtonIcon(BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_arrow_back_wht))
+                            .build()
+                            .launchUrl((Activity) mContext, Uri.parse(item.url));
+                } else {
+                    mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(item.url)));
+                }
             }
         });
 
@@ -89,10 +113,12 @@ public final class MalshabCoursesAdapter extends BaseAdapter {
     private static class Item {
         public final int name;
         public final String image;
+        public final String url;
 
-        Item(int name, String image) {
+        Item(int name, String image, String url) {
             this.name = name;
             this.image = image;
+            this.url = url;
         }
     }
 }
