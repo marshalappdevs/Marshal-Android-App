575ee30297964b22219cdf5ce4e9f6aa3b2ba8c6
shnizlon <orshnizelon@gmail.com>
2016-05-31 09:02:53 +0300

    cleanups

--- app/src/main/java/com/basmach/marshal/ui/fragments/ErrorFragment.java
+++ app/src/main/java/com/basmach/marshal/ui/fragments/ErrorFragment.java
@@ -1,6 +1,5 @@
 package com.basmach.marshal.ui.fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,22 +8,13 @@ import android.view.ViewGroup;
 
 import com.basmach.marshal.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ErrorFragment extends Fragment {
-
-
-    public ErrorFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_error, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_error, container, false);
+        return rootView;
     }
 
 }
