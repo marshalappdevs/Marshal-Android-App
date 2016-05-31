080dc1f2a5a4201061ed75c4b049b933bbc87b22
shnizlon <orshnizelon@gmail.com>
2016-05-31 14:34:30 +0300

    broadcast to all courses activity

--- app/src/main/java/com/basmach/marshal/ui/ShowAllCoursesActivity.java
+++ app/src/main/java/com/basmach/marshal/ui/ShowAllCoursesActivity.java
@@ -1,5 +1,9 @@
 package com.basmach.marshal.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,10 +27,12 @@ public class ShowAllCoursesActivity extends AppCompatActivity {
     public static final String EXTRA_COURSES_LIST = "courses_list";
     public static final String EXTRA_COURSE_TYPE = "course_type";
     public static final String EXTRA_RATINGS_LIST = "extra_ratings";
+    private BroadcastReceiver mAdaptersBroadcastReceiver;
 
     RecyclerView mRecyclerView;
     GridLayoutManager mGridLayoutManager;
     CoursesRecyclerAdapter mAdapter;
+    Toolbar mToolbar;
 
     String mCoursesType;
     ArrayList<Course> mCourses;
@@ -43,15 +49,15 @@ public class ShowAllCoursesActivity extends AppCompatActivity {
         mCourses = getIntent().getParcelableArrayListExtra(EXTRA_COURSES_LIST);
         mRatings = getIntent().getParcelableArrayListExtra(EXTRA_RATINGS_LIST);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.showAllCourses_activity_toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.showAllCourses_activity_toolbar);
 
-        if (mCoursesType != null) toolbar.setTitle(mCoursesType);
+        if (mCoursesType != null && mToolbar != null) mToolbar.setTitle(mCoursesType);
 
-        setSupportActionBar(toolbar);
+        setSupportActionBar(mToolbar);
 
         if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
@@ -70,11 +76,37 @@ public class ShowAllCoursesActivity extends AppCompatActivity {
                 mRecyclerView.setAdapter(mAdapter);
             }
         }
+        mAdaptersBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mAdapter.notifyDataSetChanged();
+            }
+        };
+
+        registerReceiver(mAdaptersBroadcastReceiver, new IntentFilter(CoursesRecyclerAdapter.ACTION_ITEM_DATA_CHANGED));
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         LocaleUtils.updateLocale(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdaptersBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mAdapter.notifyDataSetChanged();
+            }
+        };
+        registerReceiver(mAdaptersBroadcastReceiver, new IntentFilter(CoursesRecyclerAdapter.ACTION_ITEM_DATA_CHANGED));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mAdaptersBroadcastReceiver);
     }
 }
