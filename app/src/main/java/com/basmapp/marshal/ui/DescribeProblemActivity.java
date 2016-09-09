70c274100558a1726fc4a2c8636e21104832826c
marshaldevs <nfZ!9DKi?&r?3/FUWL1Q>
2016-09-09 13:17:23 +0300

    minor change

--- app/src/main/java/com/basmapp/marshal/ui/DescribeProblemActivity.java
+++ app/src/main/java/com/basmapp/marshal/ui/DescribeProblemActivity.java
@@ -176,7 +176,7 @@ public class DescribeProblemActivity extends BaseActivity {
                     Uri uri = data.getData();
                     if (getRealPathFromURI(uri) != null) {
                         try {
-                            attachments.add(0, Uri.fromFile(new File(getRealPathFromURI(uri))));
+                            attachments.add(Uri.fromFile(new File(getRealPathFromURI(uri))));
                             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                             screenshots[0].setImageBitmap(bitmap);
                             screenshots[0].setClickable(false);
@@ -193,7 +193,7 @@ public class DescribeProblemActivity extends BaseActivity {
                     Uri uri = data.getData();
                     if (getRealPathFromURI(uri) != null) {
                         try {
-                            attachments.add(1, Uri.fromFile(new File(getRealPathFromURI(uri))));
+                            attachments.add(Uri.fromFile(new File(getRealPathFromURI(uri))));
                             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                             screenshots[1].setImageBitmap(bitmap);
                             screenshots[1].setClickable(false);
@@ -207,7 +207,7 @@ public class DescribeProblemActivity extends BaseActivity {
                     Uri uri = data.getData();
                     if (getRealPathFromURI(uri) != null) {
                         try {
-                            attachments.add(2, Uri.fromFile(new File(getRealPathFromURI(uri))));
+                            attachments.add(Uri.fromFile(new File(getRealPathFromURI(uri))));
                             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                             screenshots[2].setImageBitmap(bitmap);
                             screenshots[2].setClickable(false);
