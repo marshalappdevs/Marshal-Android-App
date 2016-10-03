e123f288b9a64852bbdf175d640d7d5625e0c009
marshaldevs <marshaldevs@gmail.com>
2016-10-04 00:27:31 +0300

    minor changes and fixes

--- app/src/main/java/com/basmapp/marshal/util/ThemeUtils.java
+++ app/src/main/java/com/basmapp/marshal/util/ThemeUtils.java
@@ -4,11 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-import android.util.Log;
 
 import com.basmapp.marshal.Constants;
 import com.basmapp.marshal.R;
@@ -17,6 +16,48 @@ import com.basmapp.marshal.ui.MainActivity;
 public class ThemeUtils {
 
     public static void updateTheme(AppCompatActivity appCompatActivity) {
+        int[] PRIMARY_COLORS = new int[]{
+                ContextCompat.getColor(appCompatActivity, R.color.red_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.pink_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.purple_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.deep_purple_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.indigo_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.blue_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.light_blue_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.cyan_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.teal_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.green_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.light_green_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.lime_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.yellow_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.amber_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.orange_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.deep_orange_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.brown_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.grey_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.blue_grey_primary_color),
+                ContextCompat.getColor(appCompatActivity, R.color.black_primary_color)
+        };
+
+        int[] ACCENT_COLORS = new int[]{
+                ContextCompat.getColor(appCompatActivity, R.color.red_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.pink_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.purple_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.deep_purple_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.indigo_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.blue_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.light_blue_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.cyan_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.teal_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.green_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.light_green_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.lime_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.yellow_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.amber_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.orange_accent_color),
+                ContextCompat.getColor(appCompatActivity, R.color.deep_orange_accent_color),
+        };
+
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(appCompatActivity);
         String theme = sharedPreferences.getString(Constants.PREF_THEME, "light");
         switch (theme) {
@@ -32,85 +73,86 @@ public class ThemeUtils {
         }
         appCompatActivity.getDelegate().applyDayNight();
         int primaryColorCode = MainActivity.getPrimaryColorCode(appCompatActivity);
-        if (primaryColorCode == Color.parseColor("#F44336")) {
+        if (primaryColorCode == PRIMARY_COLORS[0]) {
             appCompatActivity.setTheme(R.style.AppTheme_Red);
-        } else if (primaryColorCode == Color.parseColor("#E91E63")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[1]) {
             appCompatActivity.setTheme(R.style.AppTheme_Pink);
-        } else if (primaryColorCode == Color.parseColor("#9C27B0")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[2]) {
             appCompatActivity.setTheme(R.style.AppTheme_Purple);
-        } else if (primaryColorCode == Color.parseColor("#673AB7")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[3]) {
             appCompatActivity.setTheme(R.style.AppTheme_Deep_Purple);
-        } else if (primaryColorCode == Color.parseColor("#3F51B5")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[4]) {
             appCompatActivity.setTheme(R.style.AppTheme_Indigo);
-        } else if (primaryColorCode == Color.parseColor("#2196F3")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[5]) {
             appCompatActivity.setTheme(R.style.AppTheme_Blue);
-        } else if (primaryColorCode == Color.parseColor("#03A9F4")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[6]) {
             appCompatActivity.setTheme(R.style.AppTheme_Light_Blue);
-        } else if (primaryColorCode == Color.parseColor("#00BCD4")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[7]) {
             appCompatActivity.setTheme(R.style.AppTheme_Cyan);
-        } else if (primaryColorCode == Color.parseColor("#009688")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[8]) {
             appCompatActivity.setTheme(R.style.AppTheme_Teal);
-        } else if (primaryColorCode == Color.parseColor("#4CAF50")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[9]) {
             appCompatActivity.setTheme(R.style.AppTheme_Green);
-        } else if (primaryColorCode == Color.parseColor("#8BC34A")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[10]) {
             appCompatActivity.setTheme(R.style.AppTheme_Light_Green);
-        } else if (primaryColorCode == Color.parseColor("#CDDC39")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[11]) {
             appCompatActivity.setTheme(R.style.AppTheme_Lime);
-        } else if (primaryColorCode == Color.parseColor("#FFEB3B")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[12]) {
             appCompatActivity.setTheme(R.style.AppTheme_Yellow);
-        } else if (primaryColorCode == Color.parseColor("#FFC107")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[13]) {
             appCompatActivity.setTheme(R.style.AppTheme_Amber);
-        } else if (primaryColorCode == Color.parseColor("#FF9800")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[14]) {
             appCompatActivity.setTheme(R.style.AppTheme_Orange);
-        } else if (primaryColorCode == Color.parseColor("#FF5722")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[15]) {
             appCompatActivity.setTheme(R.style.AppTheme_Deep_Orange);
-        } else if (primaryColorCode == Color.parseColor("#795548")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[16]) {
             appCompatActivity.setTheme(R.style.AppTheme_Brown);
-        } else if (primaryColorCode == Color.parseColor("#9E9E9E")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[17]) {
             appCompatActivity.setTheme(R.style.AppTheme_Grey);
-        } else if (primaryColorCode == Color.parseColor("#607D8B")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[18]) {
             appCompatActivity.setTheme(R.style.AppTheme_Blue_Grey);
-        } else if (primaryColorCode == Color.parseColor("#000000")) {
+        } else if (primaryColorCode == PRIMARY_COLORS[19]) {
             appCompatActivity.setTheme(R.style.AppTheme_Black);
         }
         int accentColorCode = MainActivity.getAccentColorCode(appCompatActivity);
-        if (accentColorCode == Color.parseColor("#FF5252")) {
+        if (accentColorCode == ACCENT_COLORS[0]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorRed, true);
-        } else if (accentColorCode == Color.parseColor("#FF4081")) {
+        } else if (accentColorCode == ACCENT_COLORS[1]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorPink, true);
-        } else if (accentColorCode == Color.parseColor("#E040FB")) {
+        } else if (accentColorCode == ACCENT_COLORS[2]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorPurple, true);
-        } else if (accentColorCode == Color.parseColor("#7C4DFF")) {
+        } else if (accentColorCode == ACCENT_COLORS[3]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorDeepPurple, true);
-        } else if (accentColorCode == Color.parseColor("#536DFE")) {
+        } else if (accentColorCode == ACCENT_COLORS[4]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorIndigo, true);
-        } else if (accentColorCode == Color.parseColor("#448AFF")) {
+        } else if (accentColorCode == ACCENT_COLORS[5]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorBlue, true);
-        } else if (accentColorCode == Color.parseColor("#40C4FF")) {
+        } else if (accentColorCode == ACCENT_COLORS[6]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorLightBlue, true);
-        } else if (accentColorCode == Color.parseColor("#18FFFF")) {
+        } else if (accentColorCode == ACCENT_COLORS[7]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorCyan, true);
-        } else if (accentColorCode == Color.parseColor("#64FFDA")) {
+        } else if (accentColorCode == ACCENT_COLORS[8]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorTeal, true);
-        } else if (accentColorCode == Color.parseColor("#69F0AE")) {
+        } else if (accentColorCode == ACCENT_COLORS[9]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorGreen, true);
-        } else if (accentColorCode == Color.parseColor("#B2FF59")) {
+        } else if (accentColorCode == ACCENT_COLORS[10]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorLightGreen, true);
-        } else if (accentColorCode == Color.parseColor("#EEFF41")) {
+        } else if (accentColorCode == ACCENT_COLORS[11]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorLime, true);
-        } else if (accentColorCode == Color.parseColor("#FFFF00")) {
+        } else if (accentColorCode == ACCENT_COLORS[12]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorYellow, true);
-        } else if (accentColorCode == Color.parseColor("#FFD740")) {
+        } else if (accentColorCode == ACCENT_COLORS[13]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorAmber, true);
-        } else if (accentColorCode == Color.parseColor("#FFAB40")) {
+        } else if (accentColorCode == ACCENT_COLORS[14]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorOrange, true);
-        } else if (accentColorCode == Color.parseColor("#FF6E40")) {
+        } else if (accentColorCode == ACCENT_COLORS[15]) {
             appCompatActivity.getTheme().applyStyle(R.style.OverrideAccentColorDeepOrange, true);
         }
     }
+
     public static int getThemeColor(Context context, int id) {
         Resources.Theme theme = context.getTheme();
-        TypedArray a = theme.obtainStyledAttributes(new int[] { id });
+        TypedArray a = theme.obtainStyledAttributes(new int[]{id});
         int result = a.getColor(0, 0);
         a.recycle();
         return result;
