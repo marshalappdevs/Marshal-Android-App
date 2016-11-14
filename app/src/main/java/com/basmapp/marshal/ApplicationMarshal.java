09966506838e2be792673dc3ce909c17e8a80ec4
marshaldevs <marshaldevs@gmail.com>
2016-11-14 13:01:44 +0200

    improvement

--- app/src/main/java/com/basmapp/marshal/ApplicationMarshal.java
+++ app/src/main/java/com/basmapp/marshal/ApplicationMarshal.java
@@ -3,17 +3,22 @@ package com.basmapp.marshal;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.content.res.Configuration;
 import android.preference.PreferenceManager;
+import android.util.Base64;
 import android.util.Log;
 
 import com.basmapp.marshal.services.UpdateIntentService;
-import com.basmapp.marshal.util.ContentProvider;
 import com.basmapp.marshal.util.LocaleUtils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 public class ApplicationMarshal extends Application {
+    private static final String SIGNATURE = "ndtzzSJ5ftzT6vSiJH20aMp8/m8=";
 
     @Override
     public void onCreate() {
@@ -23,6 +28,9 @@ public class ApplicationMarshal extends Application {
         LocaleUtils.updateConfig(this, getBaseContext().getResources().getConfiguration());
         UpdateIntentService.startCheckForUpdate(getApplicationContext());
 //        ContentProvider.getInstance().initAllData(getApplicationContext());
+        if (!isValidSignature(getApplicationContext())) {
+            System.exit(0);
+        }
     }
 
     @Override
@@ -39,5 +47,28 @@ public class ApplicationMarshal extends Application {
     public static void switchNewUpdatesToShowSetting(Context context, boolean state) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         sharedPreferences.edit().putBoolean(Constants.PREF_IS_THERE_UPDATES_TO_SHOW, state).apply();
+    }
+
+    // Check if package manager signature equals to basmapp_keystore_release signature,
+    // protect app from being signed and installed with different key
+    public static boolean isValidSignature(Context context) {
+        int flag = PackageManager.GET_SIGNATURES;
+        try {
+            Signature[] signatures = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), flag).signatures;
+            for (Signature signature : signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String currentSignature = Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                // Compare signatures
+                if (SIGNATURE.equals(currentSignature)) {
+                    return true;
+                }
+            }
+        } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
+            // Issue in checking signature
+            e.printStackTrace();
+        }
+        return false;
     }
 }
