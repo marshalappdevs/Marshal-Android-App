df61e0d57b2b7a52979269afae4d08c206dfca9b
marshaldevs <marshaldevs@gmail.com>
2016-11-26 13:16:13 +0200

    added the ability to search for course before end date and after start date

--- app/src/main/java/com/basmapp/marshal/ui/SearchActivity.java
+++ app/src/main/java/com/basmapp/marshal/ui/SearchActivity.java
@@ -223,7 +223,17 @@ public class SearchActivity extends BaseActivity {
         });
         mSearchView.clearFocus();
 
+
         // Show filtered search if dates are available (from saved instance for example)
+        // Searching for courses after start date without end date limit
+        if (mFinalStartDate != 0 && mFinalEndDate == 0) {
+            filterByDatesRange(mFinalStartDate, 0);
+        }
+        // Searching for courses before end date without start date limit
+        if (mFinalEndDate != 0 && mFinalStartDate == 0) {
+            filterByDatesRange(0, mFinalEndDate);
+        }
+        // Searching for courses in a date range
         if (mFinalStartDate != 0 && mFinalEndDate != 0) {
             filterByDatesRange(mFinalStartDate, mFinalEndDate);
         }
